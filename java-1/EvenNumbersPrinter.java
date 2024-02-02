c57a7fdb89c55fba1de0e9aa54bc83ce051dad87
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-02-02 22:36:50 +0600

    even numbers

---
+++ java-1/EvenNumbersPrinter.java
@@ -0,0 +1,10 @@
+public class EvenNumbersPrinter {
+    public static void main(String[] args) {
+        System.out.println("Even numbers between 9 and 100:");
+
+        for (int i = 10; i < 100; i +=2 ){
+            System.out.print(i + " ");
+        }
+    }
+    
+}
