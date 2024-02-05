c327fc71be19f95e65c987e60c4e9b2bb955460d
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-02-05 23:48:00 +0600

    resource leaked

---
+++ java-1/Square.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Square {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+        int square = input * input;
+        System.out.println(square);
+
+
+    }
+    
+}
