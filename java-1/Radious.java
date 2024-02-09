e3ef5c942ade745a253161e5eae8e408c4f47616
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-02-09 22:10:27 +0600

    type casting kora lagbye

--- java-1/Radious.java
+++ java-1/Radious.java
@@ -2,9 +2,10 @@ import java.util.*;
 public class Radious {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        double valueofR = sc.nextInt();
-        double radious = valueofR * 3.14;
-        System.out.println(radious);
+        float valueofR = sc.nextInt();
+        double radious =  valueofR * 3.14;
+        int radiousFinal = (int) radious;
+        System.out.println(radiousFinal);
         sc.close();
     }
     
