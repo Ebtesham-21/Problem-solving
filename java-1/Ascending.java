bbadc4f334be9f88bc3077ba3baa36d112ae9fc6
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-02-10 23:55:08 +0600

    ascending

---
+++ java-1/Ascending.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Ascending {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int input1 = sc.nextInt();
+        int input2 = sc.nextInt();
+        int input3 = sc.nextInt();
+        if(input1> input2 && input1 > input3){
+            System.out.println(input1, input2, input3);
+
+        }
+    }
+    
+}
