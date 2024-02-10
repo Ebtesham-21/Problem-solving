cb9ba838ec35b26ed4d8090168b32028e10b61f5
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-02-11 00:10:25 +0600

    prb asye

--- java-1/Ascending.java
+++ java-1/Ascending.java
@@ -1,14 +1,24 @@
 import java.util.*;
 public class Ascending {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int input1 = sc.nextInt();
-        int input2 = sc.nextInt();
-        int input3 = sc.nextInt();
-        if(input1> input2 && input1 > input3){
-            System.out.println(input1, input2, input3);
-
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[3];
+        for(int i = 0; i < 3; i++) {
+            numbers[i] = scanner.nextInt();
         }
+
+        Arrays.sort(numbers);
+        for(int number:numbers) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println();
+
+        for (int number:numbers){
+            System.out.print(" ");
+        }
+
+        scanner.close();
     }
     
 }
