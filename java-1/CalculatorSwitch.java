fddc932c371c6a6f0787714568514f7512e9598c
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-02-14 23:08:53 +0600

    calculator switch 2

--- java-1/CalculatorSwitch.java
+++ java-1/CalculatorSwitch.java
@@ -2,8 +2,11 @@ import java.util.*;
 public class CalculatorSwitch {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter a : ");
         int a = sc.nextInt();
+        System.out.println("enter b : ");
         int b = sc.nextInt();
+        System.out.println("operator : ");
         char operator = sc.next().charAt(0);
 
         switch(operator) {
@@ -16,7 +19,7 @@ public class CalculatorSwitch {
             case '/' : System.out.println(a / b);
             break;
             default : System.out.println("wrong input or operator , please check again");
-            
+
         }
     }
     
