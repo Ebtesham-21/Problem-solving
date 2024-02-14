63ae5631ba657e1b234b55c27be0a3bfb08d2603
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-02-14 23:33:10 +0600

    alhamdulillah 5th contribution

---
+++ java-1/WhileTest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class WhileTest {
+    public static void main (String args[]){
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("I love you Shuchi");
+            counter++;
+        }
+        
+    }
+}
