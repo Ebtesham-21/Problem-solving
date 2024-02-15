bbf057d3e51d848e58e204224f93471dda2e5a35
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-02-15 23:16:20 +0600

    java update prb

---
+++ java-1/PrintN.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class PrintN {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+        int counter = 1;
+        sc.close();
+
+        while(counter < range){
+            System.out.println(counter + " ");
+            counter++;
+        }
+        System.out.println();
+        
+
+    }
+    
+    
+}
